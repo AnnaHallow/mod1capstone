323bcb20e20b35c3aeaf5457930df7af6ec7d2d1
Anna <lalonglois@gmail.com>
2022-12-13 15:22:48 -0500

    Anna

--- src/main/java/com/techelevator/Controller/TransactionController.java
+++ src/main/java/com/techelevator/Controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.techelevator.Controller;
 import com.techelevator.Exceptions.ProductNotFoundException;
 import com.techelevator.Model.Inventory;
 import com.techelevator.Model.Balance;
+import java.util.Scanner;
 
 public class TransactionController {
     public static Balance vendingBalance = Balance.getBalanceInstance();
@@ -17,7 +18,22 @@ public class TransactionController {
     }
 
     public static void purchaseProduct(){
+        Scanner scan = new Scanner(System.in);
+        if (vendingBalance.getBalance() == 0){
+            System.out.println("Please submit additional tender.");
+        }
+        else {
+            System.out.println("Enter item location: ");
+            String slotLocation = scan.nextLine();
 
+            try {
+                vendingInventory.searchInventory(slotLocation);
+            } catch (ProductNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
     }
 
     public static void finishTransaction(){
