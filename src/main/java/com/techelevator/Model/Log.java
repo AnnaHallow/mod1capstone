ccf64b792f392384ca15c255a6eafd94535a10a3
Warren <warren.pierson05@gmail.com>
2022-12-13 16:16:42 -0800

    Updated Log Class

--- src/main/java/com/techelevator/Model/Log.java
+++ src/main/java/com/techelevator/Model/Log.java
@@ -1,5 +1,23 @@
 package com.techelevator.Model;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Log {
 
+
+    public static void log(String operation, double amount, double balance) {
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        try {
+            PrintWriter transactionLog = new PrintWriter("src/main/java/com/techelevator/Log.txt");
+            transactionLog.println(date + " " + time + " " + operation + " $" + amount + "$" + balance);
+        } catch (Exception e) {
+            System.out.println("An error has occurred");
+        }
+
+
+    }
 }
